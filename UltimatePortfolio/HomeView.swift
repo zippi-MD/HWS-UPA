f6e8cbfe0d0378c2ebd527f262b358c6423dbe4e
Alejandro Mendoza <zippi180@gmail.com>
2021-11-23 17:58:57 -0600

    First steps in UI

---
+++ UltimatePortfolio/HomeView.swift
@@ -0,0 +1,30 @@
+//
+//  HomeView.swift
+//  UltimatePortfolio
+//
+//  Created by Alejandro Mendoza on 23/11/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject var dataController: DataController
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Add data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
