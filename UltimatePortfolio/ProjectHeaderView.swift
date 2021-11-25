7914c0d63c15af86fde07ea75e6bbb3869b86a71
Alejandro Mendoza <zippi180@gmail.com>
2021-11-24 23:22:04 -0600

    Adding a section header

---
+++ UltimatePortfolio/ProjectHeaderView.swift
@@ -0,0 +1,36 @@
+//
+//  ProjectHeaderView.swift
+//  UltimatePortfolio
+//
+//  Created by Alejandro Mendoza on 24/11/21.
+//
+
+import SwiftUI
+
+struct ProjectHeaderView: View {
+    @ObservedObject var project: Project
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(project.projectTitle)
+                ProgressView(value: project.completionAmount)
+                    .tint(Color(project.projectColor))
+            }
+            
+            Spacer()
+            
+            NavigationLink(destination: EmptyView()) {
+                Image(systemName: "square.and.pencil")
+                    .imageScale(.large)
+            }
+        }
+        .padding(.bottom, 10)
+    }
+}
+
+struct ProjectHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectHeaderView(project: Project.example)
+    }
+}
